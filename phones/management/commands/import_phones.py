d13012410c5c219f22a9b88e3187e6a15d637987
softarf <98207887+softarf@users.noreply.github.com>
2023-06-04 14:58:17 +0500

    Выполняет ДЗ 5.3.1 "Выгрузка каталога товаров из csv-файла в БД"
    Много преобразований.
    
    Changes committed:
        1. modified:   main/settings.py
        2. modified:   phones/management/commands/import_phones.py
        3. modified:   phones/models.py
        4. modified:   phones/views.py
        5. modified:   templates/base.html
        6. new file:   phones/migrations/0001_initial.py
        7. new file:   phones/migrations/0002_alter_phone_release_date.py
        8. new file:   phones/migrations/0003_alter_phone_lte_exists_alter_phone_release_date.py

--- phones/management/commands/import_phones.py
+++ phones/management/commands/import_phones.py
@@ -1,17 +1,45 @@
+import os
 import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
 
+def to_slug(name: str) -> str:
+    return name.lower().strip().replace(' ', '-')
+
+
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        pass
+        """Получает имя csv-файла с данными для загрузки.
+           По умолчанию - 'phones.csv'."""
+        parser.add_argument(
+            'file_name',
+            action='store',
+            nargs='?',
+            default='phones.csv',
+            help='Название файла для считывания данных'
+        )
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
-
+        """Переносит данные из csv-файла в модель (таблицу БД) Phone."""
+        input_file = options['file_name']
+        if not os.path.exists(input_file):
+            self.stdout.write(f"Файл не найден\n{input_file}")
+            return
+        with open(input_file, 'r', encoding="utf-8") as phones_file:
+            phones = list(csv.DictReader(phones_file, delimiter=';'))
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            Phone.objects.create(name=phone['name'],
+                                 image=phone['image'],
+                                 price=phone['price'],
+                                 release_date=phone['release_date'],
+                                 lte_exists=phone['lte_exists'],
+                                 # TODO: Добавить создание поля Slug
+                                 # Не дошёл до 'Преобразователя путей по умолчанию'. Сделал "в лоб", написал своё.
+                                 slug=to_slug(phone['name'])
+            )
+            # Считывание и обновление отдельного параметра.
+            # Phone.objects.filter(id=phone['id']).update(slug=to_slug(phone['name']))
+        self.stdout.write("Данные перенесены.")    # Для теста.
+        return
