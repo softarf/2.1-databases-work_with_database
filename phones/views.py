a9e25b6f589dbf8f14ebf8a3938c449903a99068
softarf <98207887+softarf@users.noreply.github.com>
2023-08-10 12:55:06 +0500

    Сохраняет прорядок сортировки при возврате на общую страницу.
    При возврате со страницы детального просмотра на общую прорядок сортировки телефонов
    сохраняется в виде, который был в последний раз.
    В шаблоне 'product.html' добавлена возможность возврата переданного атрибута
    'sorted' в виде параметра GET-запросе.
    В файле 'views.py' порядок последней сортировки 'sort_param' передаётся между
    "вьюшками" глобальной переменной 'ORDER'.
    
    Changes committed:
        1. modified:   phones/views.py
        2. modified:   templates/product.html

--- phones/views.py
+++ phones/views.py
@@ -4,21 +4,26 @@ from django.shortcuts import render, redirect, get_object_or_404
 from phones.models import Phone
 
 
+ORDER = ''
+
+
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    global ORDER
     sort_param = request.GET.get("sort", "id")
     if sort_param != 'id':
         if sort_param == 'name':
             pass
-        elif sort_param == 'max_price':
-            sort_param = '-price'
         elif sort_param == 'min_price':
             sort_param = 'price'
+        elif sort_param == 'max_price':
+            sort_param = '-price'
         else:
             return HttpResponse(f"<p style='color: red'>Неизвестный параметр сортировки: {sort_param}</p>")
+    ORDER = sort_param
     template = 'catalog.html'
     products = Phone.objects.order_by(sort_param)
     context = {'phones': products}
@@ -26,7 +31,10 @@ def show_catalog(request):
 
 
 def show_product(request, slug):
+    global ORDER
+    ORDER = 'min_price' if ORDER == 'price' else ('max_price' if ORDER == '-price' else ORDER)
     template = 'product.html'
     product = get_object_or_404(Phone, slug=slug)
-    context = {'phone': product}
+    context = {'phone': product,
+               'sorted': ORDER}
     return render(request, template, context)
